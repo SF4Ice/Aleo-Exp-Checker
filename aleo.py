137e05ab43199a3206c50713a1effd3080969b41
kenblockchainlabs <126150587+kenblockchainlabs@users.noreply.github.com>
2023-02-25 13:28:17 +0700

    Update aleo.py

--- aleo.py
+++ aleo.py
@@ -3,3 +3,14 @@ import re
 import time
 from datetime import datetime
 from bs4 import BeautifulSoup
+
+COINBASE_MIN = 1
+INCENTIVES_MIN = 0.01
+SOLUTIONS_MIN = 2
+SLEEP_TIME_SEC = 360
+
+API_TOKEN = "2109:AA-LL_EEOO"
+CHAT_ID = "-1703"
+ADDRESSES = {
+    "num1-serv1": "aleo...address1",
+    "num2-serv2": "aleo...address2",
